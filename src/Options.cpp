a7aa5ffe703604b028e77a0e0b38dff9ad84ab43
cjustin <cjustin@cjustin-G14-laptop>
2021-01-04 20:04:51 -0800

    add options header to store global options

---
+++ src/Options.cpp
@@ -0,0 +1,16 @@
+/*
+ * Options.cpp
+ *
+ *  Created on: Oct 14, 2020
+ *      Author: cjustin
+ */
+
+#include <Options.h>
+using namespace std;
+
+namespace opt {
+extern int verbose;
+extern int k;
+}
+
+
---
+++ src/Options.h
@@ -0,0 +1,22 @@
+/*
+ * Options.h
+ *
+ *  Created on: Oct 14, 2020
+ *      Author: cjustin
+ */
+
+#ifndef OPTIONS_H
+#define OPTIONS_H 1
+
+#include <stdint.h>
+#include <string>
+
+/**
+ * Global variables that are mostly constant for the duration of the
+ * execution of the program.
+ */
+namespace opt {
+int verbose = 0;
+int k = 31;
+}
+#endif
