684f688a8c6b94b42c08981b8faad151a7bc8f20
cjustin <cjustin@cjustin-G14-laptop>
2021-01-04 20:20:29 -0800

    init group building code

---
+++ src/SeqGroupBuilder.hpp
@@ -0,0 +1,22 @@
+/*
+ * SeqGroupBuilder.hpp
+ *
+ *  Created on: Jan 4, 2021
+ *      Author: cjustin
+ */
+
+#ifndef SRC_SEQGROUPBUILDER_HPP_
+#define SRC_SEQGROUPBUILDER_HPP_
+
+class SeqGroupBuilder {
+public:
+	SeqGroupBuilder() {
+		;
+	}
+	virtual ~SeqGroupBuilder() {
+		;
+	}
+};
+
+#endif /* SRC_SEQGROUPBUILDER_HPP_ */
+
