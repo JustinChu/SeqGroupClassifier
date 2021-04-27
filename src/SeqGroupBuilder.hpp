b2f29303958223d64b61a0035e53c8cbf78752fb
cjustin <cjustin@cjustin-G14-laptop>
2021-04-27 01:24:52 -0400

    change to assume no groups in the begining

--- src/SeqGroupBuilder.hpp
+++ src/SeqGroupBuilder.hpp
@@ -36,96 +36,76 @@ using namespace std;
 
 class SeqGroupBuilder {
 public:
-
-	typedef uint16_t GroupID;
-//	typedef std::tuple<GroupID, uint16_t, uint16_t> Entry; //currently only store unique entries
-	struct Entry {
-		Entry() :
-				m_uniqueCount(0), m_count(0) {
-
-		}
-//		Entry(uint16_t uniqueCount, uint16_t count) :
-//				m_uniqueCount(uniqueCount), m_count(count) {
-//		}
-		uint16_t m_uniqueCount;
-		uint16_t m_count;
-
-	};
-	typedef tsl::robin_map<uint64_t, shared_ptr<vector<Entry>>> GroupHash;
+	typedef tsl::robin_map<uint64_t, shared_ptr<vector<opt::Count>>> CountsHash;
 
 //	static const GroupID s_repeatID = std::numeric_limits<GroupID>::max();
 //	static const GroupID s_bgID = std::numeric_limits<GroupID>::max() - 1;
 
 	SeqGroupBuilder(const vector<string> &filenames) :
-			m_filenames(filenames), m_sampleCounts(filenames.size(), 0) {
+			m_filenames(filenames) {
 	}
 
-	GroupHash loadFiles() {
-		GroupHash tbl;
-		//load in file 1 thread per file
-		GroupID maxCount = m_filenames.size();
-
+	CountsHash loadFiles() {
+		CountsHash counts;
 		//load in BF
 		BloomFilter bf(opt::bf);
 		assert(bf.getKmerSize() == opt::k);
 //		assert(bf.getHashNum() == opt::hashNum);
 		opt::hashNum = bf.getHashNum();
 
+		unsigned sampleCounts = 0;
+
 #pragma omp parallel for
-		for (GroupID id = 0; id < maxCount; ++id) {
+		for (unsigned i = 0; i < m_filenames.size(); ++i) {
 			gzFile fp;
-			fp = gzopen(m_filenames[id].c_str(), "r");
+			fp = gzopen(m_filenames[i].c_str(), "r");
 			if (fp == Z_NULL) {
-				std::cerr << "file " << m_filenames[id] << " cannot be opened"
+				std::cerr << "file " << m_filenames[i] << " cannot be opened"
 						<< std::endl;
 				exit(1);
 			} else if (opt::verbose) {
-				std::cerr << "Opening " << m_filenames[id] << std::endl;
+				std::cerr << "Opening " << m_filenames[i] << std::endl;
 			}
 			kseq_t *seq = kseq_init(fp);
 			int l = kseq_read(seq);
 			while (l >= 0) {
 #pragma omp atomic
-				m_sampleCounts[id]++;
+				sampleCounts++;
 				l = kseq_read(seq);
 			}
 			gzclose(fp);
+		}
 
-			//reopen
-			fp = gzopen(m_filenames[id].c_str(), "r");
-			seq = kseq_init(fp);
-			l = kseq_read(seq);
+		m_sampleIDs.reserve(sampleCounts);
 
-			GroupHash uniqueEntries;
-			vector<string> ids;
-
+#pragma omp parallel for
+		for (unsigned i = 0; i < m_filenames.size(); ++i) {
+			gzFile fp;
+			fp = gzopen(m_filenames[i].c_str(), "r");
+			if (fp == Z_NULL) {
+				std::cerr << "file " << m_filenames[i] << " cannot be opened"
+						<< std::endl;
+				exit(1);
+			} else if (opt::verbose) {
+				std::cerr << "Opening " << m_filenames[i] << std::endl;
+			}
+			kseq_t *seq = kseq_init(fp);
+			int l = kseq_read(seq);
 			unsigned index = 0;
 			while (l >= 0) {
-				ids.push_back(seq->name.s);
+				m_sampleIDs.push_back(seq->name.s);
 				//k-merize and insert
 				for (ntHashIterator itr(seq->seq.s, opt::hashNum, opt::k);
 						itr != itr.end(); ++itr) {
 					//if kmer exists inside bg set
 					if (!bf.contains(*itr)) {
-						if (!tbl.contains((*itr)[0])) {
-#pragma omp critical(tbl)
-							tbl[(*itr)[0]] = shared_ptr<vector<Entry>>(
-									new vector<Entry>(m_filenames.size()));
+						if (!counts.contains((*itr)[0])) {
+#pragma omp critical(counts)
+							counts[(*itr)[0]] = shared_ptr<vector<opt::Count>>(
+									new vector<opt::Count>(sampleCounts));
 						}
-						if (!uniqueEntries.contains((*itr)[0])) {
-							uniqueEntries[(*itr)[0]] =
-									shared_ptr<vector<Entry>>(
-											new vector<Entry>(
-													m_sampleCounts[id]));
-						}
-						if((*uniqueEntries[(*itr)[0]])[index].m_uniqueCount == 0){
-							++(*uniqueEntries[(*itr)[0]])[index].m_uniqueCount;
 #pragma omp atomic
-							++(*tbl[(*itr)[0]])[id].m_uniqueCount;
-						}
-						++(*uniqueEntries[(*itr)[0]])[index].m_count;
-#pragma omp atomic
-						++(*tbl[(*itr)[0]])[id].m_count;
+						++(*counts[(*itr)[0]])[index];
 					}
 				}
 				l = kseq_read(seq);
@@ -133,91 +113,74 @@ public:
 			}
 			kseq_destroy(seq);
 			gzclose(fp);
-
-			if (m_sampleCounts.at(id) > 1) {
-				unsigned matSize = (m_sampleCounts.at(id)
-						* (m_sampleCounts.at(id) - 1)) / 2;
-
-				//create similarity matrix (jaccard)
-				int *countMat = new int[matSize];
-				//init array
-				for (unsigned i = 0; i < matSize; ++i) {
-					countMat[i] = 0;
-				}
-
-				//populate count matrix
-				vector<uint64_t> groupCounts(m_sampleCounts.at(id), 0);
-
-				for (SeqGroupBuilder::GroupHash::iterator itr =
-						uniqueEntries.begin(); itr != uniqueEntries.end();
-						++itr) {
-					uint16_t lastCount = itr->second->at(0).m_count;
-					bool allSame = true;
-					for (unsigned i = 1; i < m_sampleCounts.at(id); ++i) {
-						if(itr->second->at(i).m_count != lastCount){
-							allSame = false;
-							break;
-						}
-					}
-					if (!allSame) {
-						for (unsigned i = 0; i < m_sampleCounts.at(id); ++i) {
-							if (itr->second->at(i).m_count != 0) {
-								for (unsigned j = i + 1;
-										j < m_sampleCounts.at(id); ++j) {
-									if (itr->second->at(j).m_count
-											== itr->second->at(i).m_count) {
-										++countMat[Util::matToIndex(i, j,
-												m_sampleCounts.at(id))];
-									}
-								}
-								++groupCounts[i];
-							}
-						}
-					}
-				}
-
-				//create similarity matrix (jaccard)
-				double *distMat = new double[matSize];
-				//init array
-				for (unsigned i = 0; i < matSize; ++i) {
-					distMat[i] = 0;
-				}
-
-				for (unsigned i = 0; i < m_sampleCounts.at(id); ++i) {
-					for (unsigned j = i + 1; j < m_sampleCounts.at(id); ++j) {
-
-						unsigned setSize = groupCounts.at(i) + groupCounts.at(j)
-								- countMat[Util::matToIndex(i, j,
-										m_sampleCounts.at(id))];
-						distMat[Util::matToIndex(i, j, m_sampleCounts.at(id))] =
-								double(
-										countMat[Util::matToIndex(i, j,
-												m_sampleCounts.at(id))])
-										/ double(setSize);
-
-//						cout << Util::matToIndex(i, j, m_sampleCounts.at(id))
-//								<< "\t" << i << "\t" << j << "\t"
-//								<< double(
-//										countMat[Util::matToIndex(i, j,
-//												m_sampleCounts.at(id))])
-//										/ double(setSize) << "\t" << setSize
-//								<< endl;
-					}
-				}
-
-				//output matrix
-				printMatrix(distMat,ids, m_filenames[id] + ".mat.tsv");
-
-				//output sample to matrix table
-				printTable(uniqueEntries,ids, m_filenames[id] + ".count.tsv");
-			}
-
 		}
-		return tbl;
+		return counts;
 	}
 
-	const vector<uint16_t>& getSampleCount() {
-		return m_sampleCounts;
+	void printMatrixes(const CountsHash &counts, ) {
+		unsigned matSize = (m_sampleIDs.size() * (m_sampleIDs.size() - 1)) / 2;
+
+		//create similarity matrix (jaccard)
+		int *countMat = new int[matSize];
+		//init array
+		for (unsigned i = 0; i < matSize; ++i) {
+
+			countMat[i] = 0;
+		}
+
+		//populate count matrix
+		vector<uint64_t> groupCounts(m_sampleIDs.size(), 0);
+
+		for (CountsHash::iterator itr = counts.begin(); itr != counts.end();
+				++itr) {
+			uint16_t lastCount = itr->second->at(0);
+			bool allSame = true;
+			for (unsigned i = 1; i < m_sampleIDs.size(); ++i) {
+				if (itr->second->at(i) != lastCount) {
+					allSame = false;
+					break;
+				}
+			}
+			if (!allSame) {
+				for (unsigned i = 0; i < m_sampleIDs.size(); ++i) {
+					if (itr->second->at(i) != 0) {
+						for (unsigned j = i + 1; j < m_sampleIDs.size(); ++j) {
+							if (itr->second->at(j) == itr->second->at(i)) {
+								++countMat[Util::matToIndex(i, j, m_sampleIDs.size())];
+							}
+						}
+						++groupCounts[i];
+					}
+				}
+			}
+		}
+
+		//create similarity matrix (jaccard)
+		double *distMat = new double[matSize];
+		//init array
+		for (unsigned i = 0; i < matSize; ++i) {
+			distMat[i] = 0;
+		}
+
+		for (unsigned i = 0; i < m_sampleIDs.size(); ++i) {
+			for (unsigned j = i + 1; j < m_sampleIDs.size(); ++j) {
+
+				unsigned setSize =
+						groupCounts.at(i) + groupCounts.at(j)
+								- countMat[Util::matToIndex(i, j,
+										m_sampleIDs.size())];
+				distMat[Util::matToIndex(i, j, m_sampleIDs.size())] = double(
+						countMat[Util::matToIndex(i, j, m_sampleIDs.size())])
+						/ double(setSize);
+
+			}
+
+			//output matrix
+			printMatrix(distMat, m_sampleIDs, opt::outputPrefix + ".mat.tsv");
+
+			//output sample to matrix table
+			printTable(counts, m_sampleIDs, opt::outputPrefix + ".count.tsv");
+		}
 	}
 
 	virtual ~SeqGroupBuilder() {
@@ -226,7 +189,7 @@ public:
 
 private:
 	const vector<string> &m_filenames;
-	vector<uint16_t> m_sampleCounts;
+	vector<string> m_sampleIDs;
 
 	/*
 	 * prints to file a tab separated distance matrix
@@ -262,17 +225,17 @@ private:
 	/*
 	 * prints to file a tab separated matrix of k-mer counts
 	 */
-	void printTable(const SeqGroupBuilder::GroupHash &uniqueEntries,
+	void printTable(const CountsHash &uniqueEntries,
 			const vector<string> &ids, string outputFilename) const {
 		ofstream out(outputFilename.c_str());
 		out << "sample";
 		size_t count = 0;
-		for (SeqGroupBuilder::GroupHash::const_iterator itr =
+		for (CountsHash::const_iterator itr =
 				uniqueEntries.begin(); itr != uniqueEntries.end(); ++itr) {
-			opt::Count lastCount = itr->second->at(0).m_count;
+			opt::Count lastCount = itr->second->at(0);
 			bool allSame = true;
 			for (unsigned i = 1; i < itr->second->size(); ++i) {
-				if (itr->second->at(i).m_count != lastCount) {
+				if (itr->second->at(i) != lastCount) {
 					allSame = false;
 					break;
 				}
@@ -285,27 +248,26 @@ private:
 
 		for (unsigned i = 0; i < ids.size(); ++i) {
 			out << ids.at(i);
-			for (SeqGroupBuilder::GroupHash::const_iterator itr =
+			for (CountsHash::const_iterator itr =
 					uniqueEntries.begin(); itr != uniqueEntries.end(); ++itr) {
-				opt::Count lastCount = itr->second->at(0).m_count;
+				opt::Count lastCount = itr->second->at(0);
 				bool allSame = true;
 				for (unsigned i = 1; i < itr->second->size(); ++i) {
-					if (itr->second->at(i).m_count != lastCount) {
+					if (itr->second->at(i) != lastCount) {
 						allSame = false;
 						break;
 					}
 				}
 				if (!allSame) {
-					out << "," << itr->second->at(i).m_count;
+					out << "," << itr->second->at(i);
 				}
 			}
 			out << "\n";
 		}
 	}
 
-
-
-};
+}
+;
 
 #endif /* SRC_SEQGROUPBUILDER_HPP_ */
 
